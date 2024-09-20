71efb8012e3abfae109b10bf499d94d31ecc67bb
Prathmesh Dilip Borate <prathmeshborate01@gmail.com>
2024-09-20 15:29:50 +0000

    comparison operators

---
+++ 01_basicx/04_comparison.js
@@ -0,0 +1,19 @@
+"use strict"
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+
+// === "strict check" cheks both datatypes and data
+
+console.log("2" === 2);
