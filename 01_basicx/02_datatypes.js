1ae8fafefb1a970e328eab3f999af310654a9e2e
Prathmesh Dilip Borate <prathmeshborate01@gmail.com>
2024-09-20 14:21:07 +0000

    datatypes

---
+++ 01_basicx/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict" //treat all JS code as newer version
+
+//alert(3+3) //we are using node JS not browser
+
+console.log(3+3)
+
+
+let name = "Prathmesh"
+let age = 23
+let isLoggedIn = true
+
+// number => 2 to the power of 53
+// bigint =>
+// string => ""
+// boolean => true/false
+// null  => standalone value
+// undefined
+// symbol => defines uniqueness, mostly used in react
+
+
+// object 
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
