e68a93278d526fce2123a9f53e2be8c89fe5b98b
Prathmesh Dilip Borate <prathmeshborate01@gmail.com>
2024-09-27 15:08:49 +0000

    for loop

---
+++ 05_iterations/for.js
@@ -0,0 +1,54 @@
+//for
+
+/*
+for (let i = 0; i <= 5; i++) {
+    if (i == 5) {
+        console.log("5 is lucky number");
+    }
+    console.log(i);
+}
+*/
+
+/*
+for (let i = 1; i <= 10; i++) {
+        // console.log(`Outer loop value: ${i}`);
+        // console.log(`Table of ${i}`);
+        
+    for (let j = 1; j <= 10; j++) {
+        // console.log(`Inner loop value: ${j} and inner loop ${i}`);
+
+        // console.log(`${i} * ${j} = ${i*j}`);
+    }
+    //console.log("");
+}
+*/
+
+/*
+let myArray = ["flash", "batman", "superman"]
+for (let i = 0; i < myArray.length; i++) {
+    const element = myArray[i];
+    console.log(element);
+}
+*/
+
+//break and continue
+
+/*
+for (let i = 1; i <= 20; i++) {
+    if (i == 5) {
+        console.log("Detected 5");
+        break
+    }
+    console.log(`value of i is: ${i}`);
+}
+*/
+
+/*
+for (let i = 1; i <= 20; i++) {
+    if (i == 5) {
+        console.log("Detected 5");
+        continue
+    }
+    console.log(`value of i is: ${i}`);
+}
+*/
